677426387b949a318fbbff683a2b457d735d801e
Kaan <kaanasutay10@gmail.com>
2024-05-05 02:04:25 +0300

    FINISH Toast Notification

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitte
 let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!'
 let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again'
 
-
 function showToast(msg) {
    let toast = document.createElement('div');
    toast.classList.add('toast');
@@ -21,5 +20,5 @@ function showToast(msg) {
    setTimeout(() => {
     toast.remove();
    }, 6000);
-
+ 
 }
